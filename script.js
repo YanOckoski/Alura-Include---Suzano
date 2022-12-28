7927f6891bdef2e0a91da086ab8c669c60108689
Yan Ockoski Alves <108777359+YanOckoski@users.noreply.github.com>
2022-12-28 20:21:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+/*--- menu mobile ---*/
+const MENU = document.querySelector('#menu-mobile')
+const ICONE_MENU = document.querySelector('.icone-menu')
+const EXIT = document.querySelector('#menu-exit')
+
+function abrirMenuMobile() {
+    MENU.classList.remove('display-menu')
+}
+
+function fecharMenuMobile() {
+    MENU.classList.add('display-menu')
+}
+
+ICONE_MENU.onclick = abrirMenuMobile
+EXIT.onclick = fecharMenuMobile
